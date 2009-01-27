0070952b51763ff95657d72fb117c36e4d39e4db
takebeheras <takebeheras@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-01-27 17:18:35 +0000

    falta lo de la mepa de leer y escribir.

--- PRUEABA.pas
+++ PRUEABA.pas
@@ -4,11 +4,13 @@ a,b: boolean;
 c: integer;
 e: integer;
 begin
-     read(casdasd);     
+     read(c);     
      a:= false and true;
      b:= ( false );
      c:= 4 + 7;
+read(e);
 	 c:= 5 * 8;
 	 c:= 4 / 2;
-	 e:= c;	 
+	 e:= c;
+write(asfasfa);	 
 end.
