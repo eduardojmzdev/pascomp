6ae85950007d7dfa8a498410dfd034ca02aca72f
jesus.lara.gimeno <jesus.lara.gimeno@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-01-27 17:37:14 +0000

    Prueba de union de cambios

--- src/aSintactico/AbstractSimbolTable.java
+++ src/aSintactico/AbstractSimbolTable.java
@@ -1,150 +1,141 @@
 package aSintactico;
 
 import java.util.Collection;
-import java.util.Hashtable;
 import java.util.LinkedList;
+import java.util.Hashtable;
 
 /**
- * Es una plantilla generica para tablas de simbolos de lenguajes de
- * programacion
- * 
+ * Es una plantilla generica para tablas de simbolos de lenguajes de programacion
+ *
  * @since jdk 1.6
  */
-public abstract class AbstractSimbolTable<T> {
-
-	/**
-	 * Mantiene las tablas de simbolos de los distintos ambientes de
-	 * referenciamiento. Por ejemplo: Al entrar a un procedimiento P se inserta
-	 * una nueva tabla para P donde mantiene sus datos locales. Si un
-	 * identificador no esta en la tabla actual (ej: variable global), entonces
-	 * se busca en las restantes tablas.
-	 */
-	protected LinkedList<Hashtable<String, T>> pila = null;
-
-	/**
-	 * Tabla de simbolos del ambiente de referenciamiento actual.
-	 */
-	protected Hashtable<String, T> tablaActual = null;
-
-	/** Mantiene el nivel lexico */
-	protected int nivelLexico = -1;
-
-	/** Apuntador a la proxima entrada de la tabla actual */
-	protected int offset = 0;
-
-	/**
-	 * Crea la tabla de simbolos
-	 */
-	public AbstractSimbolTable() {
-		// construye la pila
-		pila = new LinkedList<Hashtable<String, T>>();
-	}
-
-	/**
-	 * Busca un elemento recorriendo toda la pila hasta encontrarlo o no.
-	 * 
-	 * @param lexema
-	 *            nombre de la entidad a buscar
-	 * @return la entrada de tipo T o null si no encuentra nada
-	 */
-	public T buscar(String lexema) {
-		int size = pila.size();
-		Hashtable<String, T> tabla;
-		T entry = null;
-		for (int i = 0; i < size; i++) {
-			tabla = pila.get(i);
-			Collection pr = tabla.values();
-			for (int j = 0; j > pr.size(); j++) {
-				System.out.println(pr);
-			}
-			entry = tabla.get(lexema);
-			if (entry != null)
-				break;
-		}
-		return entry;
-	}
-
-	/**
-	 * Inserta en el tope de la pila una nueva tabla correspondiente a un nuevo
-	 * ambiente de referenciamiento.
-	 */
-	public void crearNivelLexico() {
-		pushTable();
-		nivelLexico++;
-		offset = 0;
-	}
-
-	/**
-	 * Elimina la tabla del tope de la pila y restaura el nivel actual.
-	 */
-	public void eliminarNivelLexico() {
-		pila.removeFirst();
-		tablaActual = pila.getFirst();
-		nivelLexico--;
-	}
-
-	/**
-	 * Busca una entrada para un identificador de la tabla actual
-	 * 
-	 * @param id
-	 *            el nombre a buscar
-	 * @return true si existe una entrada para el identificador
-	 */
-	public boolean estaDeclarado(String id) {
-		return tablaActual.containsKey(id);
-	}
-
-	/**
-	 * Obtiene el nivel lexico actual
-	 * 
-	 * @return el nivel lexico actual
-	 */
-	public int getNivelLexico() {
-		return nivelLexico;
-	}
-
-	/**
-	 * 
-	 * @return el desplazamiento actual
-	 */
-	public int getOffset() {
-		return offset;
-	}
-
-	/**
-	 * Incrementa el desplazamiento actual con el valor n
-	 * 
-	 * @param n
-	 *            incremento
-	 */
-	public void incOffset(int n) {
-		offset += n;
-	}
-
-	/**
-	 * Inicializa la tabla de simbolos global con los tipos , funciones y
-	 * constantes predefinidas que son propias del lenguaje. Las subclases deben
-	 * implementarlo
-	 */
-	public abstract void init();
-
-	/**
-	 * Inserta una entrada identificada por name
-	 * 
-	 * @param name
-	 *            nombre de la entidad
-	 * @param entry
-	 *            entrada de la tabla de simbolos
-	 */
-	public void inserta(String name, T entry) {
-		tablaActual.put(name, entry);
-	}
-
-	/**
-	 * apila una nueva tabla sin modificar el nivel lexico
-	 */
-	protected void pushTable() {
-		tablaActual = new Hashtable<String, T>();
-		pila.addFirst(tablaActual);
-	}
+public abstract class AbstractSimbolTable <T>{
+    
+    /**
+     * Mantiene las tablas de simbolos de los distintos
+     * ambientes de referenciamiento. Por ejemplo:
+     * Al entrar a un procedimiento P se inserta una nueva
+     * tabla para P donde mantiene sus datos locales.
+     * Si un identificador no esta en la tabla actual (ej: variable
+     * global), entonces se busca en las restantes tablas.
+     */
+    protected LinkedList <Hashtable<String,T>>pila = null;
+    
+    /**
+     * Tabla de simbolos del ambiente de referenciamiento
+     * actual.
+     */
+    protected Hashtable <String,T>tablaActual=null;
+    
+    /** Mantiene el nivel lexico */
+    protected int nivelLexico = -1;
+    
+    /** Apuntador a la proxima entrada de la tabla actual */
+    protected int offset = 0;
+    
+    /**
+     * Crea la tabla de simbolos
+     */
+    public AbstractSimbolTable() {
+        //construye la pila
+        pila= new LinkedList <Hashtable<String,T>>();        
+    }
+    
+    /**
+     * Obtiene el nivel lexico actual
+     *@return el nivel lexico actual
+     */
+    public int getNivelLexico(){
+        return nivelLexico;
+    }
+    
+    /**
+     * 
+     * @return el desplazamiento actual
+     */
+    public int getOffset(){
+        return offset;
+    }
+    
+    /**
+     * Incrementa el desplazamiento actual con el valor  n 
+     * @param n incremento
+     */
+    public void incOffset(int n){
+        offset += n;
+    }
+    
+    /**
+     * Inicializa la tabla de simbolos global con los tipos , funciones
+     * y constantes predefinidas que son propias del lenguaje.
+     * Las subclases deben implementarlo
+     */
+    public abstract void init();
+    
+    /**
+     * Busca una entrada para un identificador de la tabla actual
+     * @param id el nombre a buscar
+     * @return true si existe una entrada para el identificador
+     */
+    public boolean estaDeclarado(String id){
+        return tablaActual.containsKey(id);
+    }
+    
+    /**
+     * Busca un elemento recorriendo toda la pila hasta encontrarlo o no.
+     * @param lexema nombre de la entidad a buscar
+     * @return la entrada de tipo T o null si no encuentra nada
+     */
+    public T buscar(String lexema){
+        int size = pila.size();
+        Hashtable<String,T> tabla;
+        T entry = null;
+        for (int i=0; i<size ; i++){
+            tabla = pila.get(i);
+            Collection pr = tabla.values();
+            for (int j= 0; j > pr.size(); j++){
+            	System.out.println(pr);
+            }
+            entry = tabla.get(lexema);
+            if (entry != null)	break;
+        }
+        return entry;
+    }
+    
+    /**
+     * Inserta una entrada identificada por name
+     * @param name nombre de la entidad 
+     * @param entry entrada de la tabla de simbolos
+     */
+    public void inserta(String name, T entry){
+        tablaActual.put(name, entry);
+    }
+    
+    /**
+     * Inserta en el tope de la pila una nueva tabla correspondiente
+     * a un nuevo ambiente de referenciamiento.
+     */
+    public void crearNivelLexico(){        
+        pushTable();
+        nivelLexico++;
+        offset = 0;        
+    }
+    
+    /**
+     * apila una nueva tabla sin modificar el nivel lexico
+     */
+    protected void pushTable(){
+        tablaActual = new Hashtable<String,T>();
+        pila.addFirst(tablaActual);        
+    }
+    
+    /**
+     * Elimina la tabla del tope de la pila y restaura el nivel
+     * actual.
+     */
+    public void eliminarNivelLexico(){
+        pila.removeFirst();
+        tablaActual = pila.getFirst();
+        nivelLexico--;
+    }
 }
--- src/aSintactico/EntryTable.java
+++ src/aSintactico/EntryTable.java
@@ -1,9 +1,9 @@
+
 package aSintactico;
 
-import java.util.LinkedList;
+import aSintactico.tipos.*;
 
-import aSintactico.tipos.Tipo;
-import aSintactico.tipos.TipoFactory;
+import java.util.LinkedList;
 
 /**
  * Implementa la tabla de simbolos para almacenar objetos Entry
@@ -12,123 +12,110 @@ import aSintactico.tipos.TipoFactory;
  * @see aSintactico.AbstractSimbolTable
  * @see aSintactico.Entry
  */
-public final class EntryTable extends AbstractSimbolTable<Entry> {
+public final class EntryTable extends AbstractSimbolTable<Entry>{
+    
+    /** Constructor */
+    public EntryTable() {
+        super();
+        
+    }
 
-	/** Constructor */
-	public EntryTable() {
-		super();
-
-	}
-
-	/**
-	 * * Inserta una entrada que corresponde a una constante * @param id nombre
-	 * de la constante * @param e entrada
-	 */
-	public void agregarConstante(Entry e) {
-		e.asignable = false;
-		inserta(e.nombre, e);
-	}
-
-	/**
-	 * Inserta una entrada que corresponde al nombre del programa
-	 * 
-	 * @param id
-	 *            nombre del programa
-	 */
-	public void agregarPrograma(String id) {
-		Entry e = new Entry(id, Entry.PROGRAMA);
-		e.asignable = false;
-		inserta(id, e);
-	}
-
-	/**
-	 * Inserta un tipo en la tabla de simbolos
-	 * 
-	 * @param id
-	 *            nombre del tipo
-	 * @param tipo
-	 *            tipo
-	 */
-	public void agregarTipo(String id, Tipo tipo) {
-		Entry e = new Entry(id, Entry.TIPO);
-		e.tipo = tipo;
-		e.asignable = false;
-		inserta(id, e);
-	}
-
-	/**
-	 * agrega una lista de variables en a la TS en n.l actual con su
-	 * correspondiente desplazamiento no negativo dependiendo de las llamadas
-	 * anteriores a este metodo
-	 * 
-	 * @param listaID
-	 *            lista de identificadores de variables
-	 * @param tipo
-	 *            tipo de las variables
-	 */
-	public void agregarVariables(LinkedList<String> listaID, Tipo tipo) {
-		int size = listaID.size();
-		Entry e;
-		String id;
-		for (int i = 0; i < size; i++) {
-			id = listaID.get(i);
-			e = new Entry(id, Entry.VARIABLE);
-			e.nivelLexico = getNivelLexico();
-			e.desplazamiento = getOffset();
-			e.tipo = tipo;
-			e.asignable = true;
-			// agrega la declaracion de la variable
-			inserta(id, e);
-			incOffset(tipo.getSize());
-		}
-	}
-
-	/**
-	 * Busca una entrada de tipo
-	 * 
-	 * @param lex
-	 * @return la entrada para el tipo o null si no existe
-	 */
-	public Entry buscarTipo(String lex) {
-		Entry entry = buscar(lex);
-		return (entry != null && entry.esTipo()) ? entry : null;
-	}
-
-	/**
-	 * Implementacion del metodo init Agrega los tipos, funciones y constantes
-	 * predefinidas del lenguaje
-	 */
-	@Override
-	public void init() {
-		if (pila.size() > 0) {
-			pila.clear();
-			if (tablaActual != null)
-				tablaActual.clear();
-		}
-
-		// crea una tabla con nivel lexico -1
-		pushTable();
-		nivelLexico = -1;
-
-		// Tipos predefinidos
-		Tipo tinteger = TipoFactory.crearTipoEntero();
-		Tipo tboolean = TipoFactory.crearTipoBooleano();
-		agregarTipo("integer", tinteger);
-		agregarTipo("boolean", tboolean);
-
-		// Constantes predefinidas
-		Entry e = new Entry("true", Entry.CONSTANTE);
-		e.asignable = false;
-		e.tipo = tboolean;
-		e.valor = Boolean.TRUE;
-		e.valorStr = "1";
-		agregarConstante(e);
-		e = new Entry("false", Entry.CONSTANTE);
-		e.asignable = false;
-		e.tipo = tboolean;
-		e.valor = Boolean.FALSE;
-		e.valorStr = "0";
-		agregarConstante(e);
-
-	}
+    /**
+     * Implementacion del metodo init
+     * Agrega los tipos, funciones y constantes predefinidas del lenguaje
+     */
+    public void init() {
+        if(pila.size()>0) {
+            pila.clear();
+            if(tablaActual!=null) tablaActual.clear();
+        }
+        
+        //crea una tabla con nivel lexico -1
+        pushTable();
+        nivelLexico = -1;
+        
+        //Tipos predefinidos
+        Tipo tinteger = TipoFactory.crearTipoEntero();
+        Tipo tboolean = TipoFactory.crearTipoBooleano();
+        agregarTipo("integer",tinteger);
+        agregarTipo("boolean",tboolean);
+        
+      //Constantes predefinidas 
+        Entry e = new Entry("true",Entry.CONSTANTE); e.asignable = false;
+        e.tipo = tboolean;
+        e.valor = Boolean.TRUE;
+        e.valorStr = "true";
+        agregarConstante(e);
+         e = new Entry("false",Entry.CONSTANTE);
+        e.asignable = false;
+        e.tipo = tboolean;
+        e.valor = Boolean.FALSE;
+        e.valorStr = "false";
+        agregarConstante(e);
+        
+    }
+    
+    /**
+     * Inserta una entrada que corresponde al nombre del programa
+     * @param id nombre del programa
+     */
+    public void agregarPrograma(String id){
+        Entry e = new Entry(id, Entry.PROGRAMA);
+        e.asignable = false;
+        inserta(id,e);
+    }
+    
+    
+    /**
+     * Inserta un tipo en la tabla de simbolos
+     * @param id nombre del tipo
+     * @param tipo tipo
+     */
+    public void agregarTipo(String id, Tipo tipo){
+        Entry e = new Entry(id,Entry.TIPO);
+        e.tipo = tipo;
+        e.asignable=false;
+        inserta(id,e);
+    }
+    
+    
+    
+    /**
+     * agrega una lista de variables en a la TS en n.l actual con su
+     * correspondiente desplazamiento no negativo dependiendo de las
+     * llamadas anteriores a este metodo
+     * @param listaID lista de identificadores de variables
+     * @param tipo tipo de las variables
+     */
+    public void agregarVariables(LinkedList<String> listaID, Tipo tipo){
+        int size = listaID.size();
+        Entry e;
+        String id;
+        for(int i=0;i<size; i++){
+            id = listaID.get(i);
+            e = new Entry(id, Entry.VARIABLE);
+            e.nivelLexico = getNivelLexico();
+            e.desplazamiento = getOffset();
+            e.tipo = tipo;
+            e.asignable=true;
+            //agrega la declaracion de la variable
+            inserta(id,e);
+            incOffset(tipo.getSize());            
+        }
+    }
+    
+    /** * Inserta una entrada que corresponde a una constante * @param id nombre de la constante * @param e entrada */ 
+    public void agregarConstante(Entry e){ 
+     e.asignable=false;
+     inserta(e.nombre,e);
+     } 
+    /**
+     * Busca una entrada de tipo     
+     * @param lex 
+     * @return la entrada para el tipo o null si no existe     
+     */
+    public Entry buscarTipo(String lex){
+        Entry entry = buscar(lex);        
+        return (entry!=null && entry.esTipo())?entry:null;        
+    }
 }
