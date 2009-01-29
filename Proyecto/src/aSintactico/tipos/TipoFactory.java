84bc3c624a8c288af207ba6c84c852be26f72103
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-01-29 21:03:55 +0000

    Version para entregar.

---
+++ Proyecto/src/aSintactico/tipos/TipoFactory.java
@@ -0,0 +1,44 @@
+package aSintactico.tipos;
+
+/**
+ * Clase TipoFactory
+ * Provee metodos para la creacion eficiente de tipos
+ *
+ */
+public final class TipoFactory {
+    /** Tipo integer*/
+    private static final TipoSimple tipoInteger = new TipoSimple(Tipo.INTEGER);
+    
+    /** Tipo boolean*/
+    private static final TipoSimple tipoBoolean = new TipoSimple(Tipo.BOOLEAN);
+    
+    /**
+     * Permite construir un tipo simlpe
+     * @param cod clase de tipo
+     * @return un tipo simple
+     */
+    public static TipoSimple crearTipoSimple(int cod){
+        if(cod == Tipo.BOOLEAN)
+            return tipoBoolean;
+        else if(cod == Tipo.INTEGER)
+            return  tipoInteger;
+        else 
+            return null;    
+    }    
+    
+    /**
+     * Crea un tipo integer
+     * @return un tipo integer
+     */
+    public static TipoSimple crearTipoEntero(){
+        return tipoInteger;        
+    }
+    
+    /**
+     * crea un tipo boolean
+     * @return un tipo boolean
+     */
+    public static TipoSimple crearTipoBooleano(){
+        return tipoBoolean;        
+    }
+}
---
+++ Proyecto/src/main/Testeable.java
@@ -0,0 +1,27 @@
+package main;
+import java.io.IOException;
+
+/**
+ * Permite crear objetos testeables.
+ * Las clases que implementen esta interface deben redefinir el 
+ * metodo <code>run</code> con el codigo propio del test.
+ * Por ejemplo, la clases AnalizadorLexico y AnalizadorSintactico implementan
+ * esta interface para poder ser testeadas por el testeador
+ * 
+ *@see Testeador
+ */
+public interface Testeable {
+    
+    /** Comienza el test   */
+    public void run()throws Exception;
+    
+    /** Setea el archivo a analizar*/
+    public void setSourceFile(String strFile)throws IOException;
+    
+    /** Finaliza el test (ej: cerrar archivos abiertos) */
+    public void finish();
+    
+    /** Comprueba si la extension del archivo es la correcta para el test */
+    public boolean validaExtension(String strFile);
+    
+}
