80c5d345e86dcd6a01a09673bdf6a1b028e9eae0
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-01-29 13:51:30 +0000

    Modificaciones en el formato del codigo.

--- src/aSintactico/tipos/TipoSimple.java
+++ src/aSintactico/tipos/TipoSimple.java
@@ -2,72 +2,79 @@ package aSintactico.tipos;
 
 /**
  * Representa un tipo simple del lenguaje. Puede ser integer, boolean y subrango
-  * @see aSintactico.tipos.TipoFactory
+ * 
+ * @see aSintactico.tipos.TipoFactory
  */
-public class TipoSimple extends Tipo{
-            
-   
-    /**
-     * Constructor 
-     * @param clase clase de tipo 
-     */
-    public TipoSimple(int clase) {
-        super(clase);
-    }
-        
-    /**
-     * Verifica si el tipo es compatible con otro
-     * @param t tipo a chequear
-     * @return true si son equivalentes, false en caso contrario
-     */
-    public boolean equivalenteCon(Tipo t) {
-        if(clase == t.clase)
-            return true;
-        else 
-            return false;
-    }
+public class TipoSimple extends Tipo {
 
-    /**
-     * Obtiene el tamaño del tipo en locaciones de memoria
-     * @return un tamaño de 1 posicion
-     */
-    public int getSize() {
-        return 1;
-    }
+	/**
+	 * Constructor
+	 * 
+	 * @param clase
+	 *            clase de tipo
+	 */
+	public TipoSimple(int clase) {
+		super(clase);
+	}
 
-    /**
-     * Obtiene la representacion del tipo como string
-     * @return el string que representa el tipo
-     */
-    public String toString(){
-        if(clase == INTEGER) return "integer";
-        if(clase == BOOLEAN) return "boolean";      
-        return "";
-    }
-    
-    /**
-     * 
-     * @return 
-     */
-    public boolean esSimple() {
-        return true;
-    }
+	/**
+	 * Verifica si el tipo es compatible con otro
+	 * 
+	 * @param t
+	 *            tipo a chequear
+	 * @return true si son equivalentes, false en caso contrario
+	 */
+	public boolean equivalenteCon(Tipo t) {
+		if (clase == t.clase)
+			return true;
+		else
+			return false;
+	}
 
-    /**
-     * 
-     * @return 
-     */
-    public boolean esEntero() {
-        return clase == INTEGER;
-    }
+	/**
+	 * Obtiene el tamaño del tipo en locaciones de memoria
+	 * 
+	 * @return un tamaño de 1 posicion
+	 */
+	public int getSize() {
+		return 1;
+	}
 
-    /**
-     * 
-     * @return 
-     */
-    public boolean esBoolean() {
-        return clase == BOOLEAN;
-    }
+	/**
+	 * Obtiene la representacion del tipo como string
+	 * 
+	 * @return el string que representa el tipo
+	 */
+	public String toString() {
+		if (clase == INTEGER)
+			return "integer";
+		if (clase == BOOLEAN)
+			return "boolean";
+		return "";
+	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean esSimple() {
+		return true;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean esEntero() {
+		return clase == INTEGER;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean esBoolean() {
+		return clase == BOOLEAN;
+	}
 
 }
--- src/main/Compilador.java
+++ src/main/Compilador.java
@@ -1,4 +1,3 @@
-
 package main;
 
 import aSintactico.AnalizadorSintactico;
@@ -6,87 +5,92 @@ import java.io.IOException;
 import mVirtual.MaquinaVirtual;
 
 /**
- * COMPILADOR 
+ * COMPILADOR
  * 
  * @see aSintactico.AnalizadorSintactico
  * @see mepa.Mepa
  * @since 1.5
  */
-public final class Compilador implements Testeable{
-    /** Analizador sintactico */
-    private AnalizadorSintactico as = null;
-    
-    /** Maquina virtual */
-    private MaquinaVirtual MV = null;
-    
-    /** Archivo a analizar */
-    private String strFile;
-   
-    /**
-     * Constructor 
-     */
-    public Compilador(){
-        as = new AnalizadorSintactico();
-    }
-    
-    /**
-     * Compila y ejecuta
-     *
-     * @throws java.lang.Exception si ocurre error en la compilacion o en la ejecucion
-     */
-    public void run() throws Exception {
-        /** ETAPA DE COMPILACION Y GEN DE CODIGO */
-        try{            
-            as.setSourceFile(strFile);
-            as.run();
-            as.finish();
-        } 
-        catch(Exception ex){
-            as.finish();
-            throw ex;
-        }
-        
-        /** ETAPA DE EJECUCION */
-        try{
-            //OBTENGO EL ARCHIVO .MV GENERADO
-            java.io.File f = as.getOutputFile();
-            
-            //PUEDE SER NULL SI COMPILO SIN GENERACION DE CODIGO
-            if(f==null) return;
-                        
-            //CREO LA MEPA Y EJECUTO EL ARCHIVO            
-            MV = new MaquinaVirtual(f.getAbsolutePath());
-            MV.run();
-            //MV.finish();
-            
-        }
-        catch(Exception ex){            
-            throw ex;            
-        }
-    }
-    
-    /**
-     * Setea el archivo a compilar
-     * @param strFile nombre de archivo 
-     * @throws java.io.IOException En este caso no ocurre excepcion
-     */
-    public void setSourceFile(String strFile) throws IOException {
-        this.strFile = strFile;        
-    }
-    
-    /** */
-    public void finish() {
-        //nada para hacer
-    }
-    
-    /**
-     * Verifica que el archivo tenga la extension correcta
-     * @param strFile nombre de archivo a compilar
-     * @return  true si el archivo soporta la extension correcta
-     * @see config.conf
-     */
-    public boolean validaExtension(String strFile) {
-        return as.validaExtension(strFile);
-    }
-    
+public final class Compilador implements Testeable {
+	/** Analizador sintactico */
+	private AnalizadorSintactico as = null;
+
+	/** Maquina virtual */
+	private MaquinaVirtual MV = null;
+
+	/** Archivo a analizar */
+	private String strFile;
+
+	/**
+	 * Constructor
+	 */
+	public Compilador() {
+		as = new AnalizadorSintactico();
+	}
+
+	/**
+	 * Compila y ejecuta
+	 * 
+	 * @throws java.lang.Exception
+	 *             si ocurre error en la compilacion o en la ejecucion
+	 */
+	public void run() throws Exception {
+		/** ETAPA DE COMPILACION Y GEN DE CODIGO */
+		try {
+			as.setSourceFile(strFile);
+			as.run();
+			as.finish();
+		} catch (Exception ex) {
+			as.finish();
+			throw ex;
+		}
+
+		/** ETAPA DE EJECUCION */
+		try {
+			// OBTENGO EL ARCHIVO .MV GENERADO
+			java.io.File f = as.getOutputFile();
+
+			// PUEDE SER NULL SI COMPILO SIN GENERACION DE CODIGO
+			if (f == null)
+				return;
+
+			// CREO LA MEPA Y EJECUTO EL ARCHIVO
+			MV = new MaquinaVirtual(f.getAbsolutePath());
+			MV.run();
+			// MV.finish();
+
+		} catch (Exception ex) {
+			throw ex;
+		}
+	}
+
+	/**
+	 * Setea el archivo a compilar
+	 * 
+	 * @param strFile
+	 *            nombre de archivo
+	 * @throws java.io.IOException
+	 *             En este caso no ocurre excepcion
+	 */
+	public void setSourceFile(String strFile) throws IOException {
+		this.strFile = strFile;
+	}
+
+	/** */
+	public void finish() {
+		// nada para hacer
+	}
+
+	/**
+	 * Verifica que el archivo tenga la extension correcta
+	 * 
+	 * @param strFile
+	 *            nombre de archivo a compilar
+	 * @return true si el archivo soporta la extension correcta
+	 * @see config.conf
+	 */
+	public boolean validaExtension(String strFile) {
+		return as.validaExtension(strFile);
+	}
+
 }
