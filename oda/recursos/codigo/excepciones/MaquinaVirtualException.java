c7dad8c704d6c00887601cadccd66e950f6d818f
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-01-29 19:49:31 +0000

    Añadido codigo fuente en el directorio recursos

---
+++ oda/recursos/codigo/excepciones/MaquinaVirtualException.java
@@ -0,0 +1,32 @@
+package excepciones;
+
+/**
+ * Representa a un error en la maquina virtual
+ * 
+ */
+public class MaquinaVirtualException extends BasicException {
+
+	/**
+	 * Identificador de version
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/** MAPEO DE CODIGOS A ERRORES */
+    static{
+        errorMap.put(new Integer(0),"Division por cero!");
+        errorMap.put(new Integer(1),"Valor invalido ingresado por teclado.");
+        errorMap.put(new Integer(2),"Magnitud no representable ingresada por teclado.");
+    }
+
+	/**
+	 * Constructor
+	 * 
+	 * @param codigo
+	 *            codigo de error
+	 */
+	public MaquinaVirtualException(int codigo) {
+		super("[MV] Error: " + errorMap.get(new Integer(codigo)));
+		this.codigo = codigo;
+	}
+
+}
