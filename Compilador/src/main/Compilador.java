6b5d5cd333810526b3ff1ec8e8b6200081afba0f
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-05-21 10:57:55 +0000

    Eliminado todo el paquete comunicacion con todas sus clases. Ahora no se usa el patron MVC.
    Hay que arreglar el paso de mensajes y errores (con excepciones).

---
+++ Compilador/src/main/Compilador.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+import aSintactico.ASintacticoImp;
+
+
+public class Compilador {
+	String ficheroEntrada;
+	String ficheroSalida;
+	
+	public Compilador(String ficheroEntrada, String ficheroSalida) {
+		this.ficheroEntrada = ficheroEntrada;
+		this.ficheroSalida = ficheroSalida;
+	}
+	
+	public void ejecutar() {
+		FileReader fuente;
+		try {
+			if (Utils.compruebaExtension(ficheroEntrada) && !ficheroSalida.equals("")) {
+
+				fuente = new FileReader(ficheroEntrada);
+
+				ASintacticoImp.getInstance().setCodigo(new PrintWriter(ficheroSalida + ".da"));
+				ASintacticoImp.getInstance().analizar(fuente);
+				System.out.println("Se ha compilador correctamente, escribiendo el fichero "
+								+ ficheroSalida + "...");
+			} else {
+				System.out.println("Error en la extensiond el fichero");
+			}
+		} catch (Exception e) {
+		}
+
+	}
+
+}
