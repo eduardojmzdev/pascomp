d5151381f0cdeede215c34fa0167c1d23b3baf86
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-05-22 19:11:15 +0000

    Modificado el main para que si no se introducen argumentos arranque el interfazo grafico y si se introducen 2 argumentos funcione como siempre.

--- trunk/Compilador/src/main/Main.java
+++ trunk/Compilador/src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import excepciones.CompiladorException;
+
 /**
  * COMPILADOR PLG 08/09
  * 
@@ -9,13 +11,38 @@ package main;
  */
 public class Main {
     public static void main(String[] args) {
-	
-	    Compilador compi= new Compilador();
-    	Ventana ventana = new Ventana(compi);
-		ventana.setTitle("PLG");
-		ventana.setSize(700, 500);
-		ventana.setVisible(true);
-	
+
+	if (args.length != 0 && args.length != 2) {
+	    System.out.println("Modo de Uso: <ficheroEntrada> <ficheroSalida>");
+	    System.out.println("Para usar el modo gráfico no introducir argumentos");
+	} else if (args.length == 2) {
+	    try {
+		String ficheroEntrada = args[0];
+		String ficheroSalida = args[1];
+
+		System.out.println("Iniciando compilador...");
+		Compilador compilador = new Compilador(ficheroEntrada, ficheroSalida);
+
+		System.out.println("Compilando fichero " + ficheroEntrada + "...");
+		compilador.ejecutar();
+
+		System.out.println("Se ha compilado correctamente, escribiendo el fichero " + ficheroSalida + "...");
+		System.out.println("Finalizado correctamente.");
+
+	    } catch (CompiladorException e) {
+		System.out.println("Se produjeron errores al compilar:");
+		System.out.println(e.getMensajeError());
+	    } catch (Exception e) {
+		System.out.println("Se produjeron errores desconocidos al compilar:");
+		e.printStackTrace();
+	    }
+	} else if (args.length == 0) {
+	    Compilador compi = new Compilador();
+	    Ventana ventana = new Ventana(compi);
+	    ventana.setTitle("PLG");
+	    ventana.setSize(700, 500);
+	    ventana.setVisible(true);
+	}
     }
 
 }
