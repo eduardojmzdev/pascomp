3478eb1b0fb4bd2c664a32c57eb2c75f5194c1b8
carlos.fernandez.aguero <carlos.fernandez.aguero@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-05-22 17:10:55 +0000

    Nacho estas tonto.

--- Compilador/src/main/Main.java
+++ Compilador/src/main/Main.java
@@ -1,7 +1,5 @@
 package main;
 
-import excepciones.CompiladorException;
-
 /**
  * COMPILADOR PLG 08/09
  * 
@@ -11,32 +9,13 @@ import excepciones.CompiladorException;
  */
 public class Main {
     public static void main(String[] args) {
-
-	if (args.length != 2) {
-	    System.out.println("Modo de Uso: <ficheroEntrada> <ficheroSalida>");
-	} else {
-	    try {
-		String ficheroEntrada = args[0];
-		String ficheroSalida = args[1];
-
-		System.out.println("Iniciando compilador...");
-		Compilador compilador = new Compilador(ficheroEntrada, ficheroSalida);
-
-		System.out.println("Compilando fichero " + ficheroEntrada + "...");
-		compilador.ejecutar();
-
-		System.out.println("Se ha compilado correctamente, escribiendo el fichero " + ficheroSalida + "...");
-		System.out.println("Finalizado correctamente.");
-
-	    } catch (CompiladorException e) {
-		System.out.println("Se produjeron errores al compilar:");
-		System.out.println(e.getMensajeError());
-	    } catch (Exception e) {
-		System.out.println("Se produjeron errores desconocidos al compilar:");
-		e.printStackTrace();
-	    }
-	}
-
+	
+	    Compilador compi= new Compilador();
+    	Ventana ventana = new Ventana(compi);
+		ventana.setTitle("PLG");
+		ventana.setSize(700, 500);
+		ventana.setVisible(true);
+	
     }
 
 }
