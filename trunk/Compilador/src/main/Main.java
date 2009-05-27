fd582640fe388ddc096e3078b29b2e08d76f34a7
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-05-27 17:05:35 +0000

    Modificado para utilizar la maquina virtual.

--- trunk/Compilador/src/main/Main.java
+++ trunk/Compilador/src/main/Main.java
@@ -11,38 +11,45 @@ import traductor.excepciones.CompiladorException;
  * @version 1.0
  */
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		if (args.length > 2) {
+			System.out.println("Con un argumento se ejecuta la máquina virtual. Ej. <ficheroEntrada>");
+			System.out.println("Con dos argumentos se ejecuta el traductor. Ej. <ficheroEntrada> <ficheroSalida>");
+			System.out.println("\nPara usar el modo gráfico no introducir argumentos");
+		} else if (args.length == 0) {
+			// Se ejecuta el interfaz gráfico
 
-	if (args.length != 0 && args.length != 2) {
-	    System.out.println("Modo de Uso: <ficheroEntrada> <ficheroSalida>");
-	    System.out.println("Para usar el modo gráfico no introducir argumentos");
-	} else if (args.length == 2) {
-	    try {
-		String ficheroEntrada = args[0];
-		String ficheroSalida = args[1];
+			Ventana ventana = new Ventana();
+			ventana.setTitle("PLG");
+			ventana.setSize(700, 500);
+			ventana.setVisible(true);
+		} else if (args.length == 1) {
+			// Se ejecuta la máquina virtual
 
-		System.out.println("Iniciando compilador...");
-		Compilador compilador = new Compilador(ficheroEntrada, ficheroSalida);
+		} else if (args.length == 2) {
+			// Se ejecuta el traductor
 
-		System.out.println("Compilando fichero " + ficheroEntrada + "...");
-		compilador.ejecutar();
+			try {
+				String ficheroEntrada = args[0];
+				String ficheroSalida = args[1];
 
-		System.out.println("Se ha compilado correctamente, escribiendo el fichero " + ficheroSalida + "...");
-		System.out.println("Finalizado correctamente.");
+				System.out.println("Iniciando compilador...");
+				Compilador compilador = new Compilador(ficheroEntrada, ficheroSalida);
 
-	    } catch (CompiladorException e) {
-		System.out.println("Se produjeron errores al compilar:");
-		System.out.println(e.getMensajeError());
-	    } catch (Exception e) {
-		System.out.println("Se produjeron errores desconocidos al compilar:");
-		e.printStackTrace();
-	    }
-	} else if (args.length == 0) {
-	    Ventana ventana = new Ventana();
-	    ventana.setTitle("PLG");
-	    ventana.setSize(700, 500);
-	    ventana.setVisible(true);
+				System.out.println("Compilando fichero " + ficheroEntrada + "...");
+				compilador.ejecutar();
+
+				System.out.println("Se ha compilado correctamente, escribiendo el fichero " + ficheroSalida + "...");
+				System.out.println("Finalizado correctamente.");
+
+			} catch (CompiladorException e) {
+				System.out.println("Se produjeron errores al compilar:");
+				System.out.println(e.getMensajeError());
+			} catch (Exception e) {
+				System.out.println("Se produjeron errores desconocidos al compilar:");
+				e.printStackTrace();
+			}
+
+		}
 	}
-    }
-
 }
