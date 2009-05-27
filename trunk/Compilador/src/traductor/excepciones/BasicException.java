51151dd852a503fe6eaf73e7ca9b5d7f9cc1f261
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-05-27 16:22:22 +0000

    Eliminadas excepciones que no se usan

--- trunk/Compilador/src/traductor/excepciones/BasicException.java
+++ trunk/Compilador/src/traductor/excepciones/BasicException.java
@@ -9,32 +9,13 @@ import java.util.HashMap;
  */
 public class BasicException extends Exception{
     
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
-	/**
-     * Map que asocia a un codigo a un mensaje de error
-     */
-    protected static final HashMap<Integer,String> errorMap = new HashMap<Integer,String>();
-    
-    /**
-     * Codigo de error, es el key del hash errorMap.
-     * Permite obtener el mensaje de error
-     */
-    protected int codigo;
+	private static final long serialVersionUID = 1L;
     
     /** Creates a new instance of BasicException */
     public BasicException(String txt) {
         super(txt);
     }
     
-    /**
-     * Permite obtener el codigo de error de la excepcion
-     * @return el codigo de error
-     */
-    public int getCodigoError(){
-        return codigo;
-    }
+
 }
