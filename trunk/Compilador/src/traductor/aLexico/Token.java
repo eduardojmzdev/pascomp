b439e50ed58fc09c3444dbac48236ed062a69afe
dani.karn <dani.karn@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-05-28 16:06:39 +0000

    ...

--- trunk/Compilador/src/traductor/aLexico/Token.java
+++ trunk/Compilador/src/traductor/aLexico/Token.java
@@ -1,80 +1,59 @@
-/**
- * Paquete que engloba las distintas clases que componen el módulo de análisis
- * léxico del lenguaje fuente del compilador.
- */
+
+ //Clase que engloba las distintas clases que componen los metodos de analisis del 
+//lenguaje fuente de nuestro compilador
+
 package traductor.aLexico;
 
-/**
- * Implementa la información requerida para cada token (categoría léxica) de los
- * que se compone la estructura léxica del lenguaje fuente.
- */
+
+ // Representa las distintas caracteristicas de nuestra clase token (categoria lexica) de los
+ // con las que se componen la estructura lexica del lenguaje fuente
+
 public class Token {
 		
-	/**
-	 * Codigo de la categoría léxica a la que pertenece el token.
-	 */
+	
+	 //Codigo de la categoria lexica a la que pertenece el token
+	 
 	private EnumToken categoria;
 	
 
 	
-	/**
-	 * Lexema que compone el token.
-	 */
+
+	  //Lexema que compone el token.
+	 
 	private String lexema;
 
 	
-	/**
-	 * Constructor principal, inicializa los componentes a utilizar
-     * en el objeto según los valores indicados.
-     * @param c Codigo de la categoría léxica.
-     * @param v Valor del token.
-     * @param l Lexema del token.
-	 */
+	
+	 //Constructor principal, inicializa los atributos
+
 	public Token(EnumToken c, String l) {
 		categoria = c;
 		lexema = l;
 	}
 	
-	/**
-	 * Constructor por defecto, inicializa los componentes a utilizar
-     * en el objeto.
-	 */
+
+	 //Constructor por defecto
+	
 	public Token() {
 		categoria = null;		
 		lexema = null;
 	}
 
-	/**
-	 * Mutador para el atributo categoría.
-	 * @param cat Codigo de la categoría léxica.
-	 */
+	//Setters y getters
+	
 	public void setCategoria(EnumToken cat){
 		categoria = cat;
 	}
 
-	/**
-	 * Accesor para el atributo categoría.
-	 * @return Valor del atributo categoria.
-	 */
+	
 	public EnumToken getCategoria(){
 		return categoria;
 	}
 	
-
-	
-	
-	/**
-	 * Mutador para el atributo lexema.
-	 * @param lex Lexema del token.
-	 */
 	public void setLexema(String lex){
 		lexema = lex;
 	}
 	
-	/**
-	 * Accesor para el atributo lexema.
-	 * @return Lexema del token.
-	 */
 	public String getLexema(){
 		return lexema;
 	}
