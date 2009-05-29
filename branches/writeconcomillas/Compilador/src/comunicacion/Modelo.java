cab2cfbf3d2775396afeb36786b701f0de8e2bcd
ignacio.fernandez.cuesta <ignacio.fernandez.cuesta@d16b3528-e942-11dd-9c15-fdd50ba39c1e>
2009-05-29 21:57:44 +0000

    No implementamos el write con comillas

--- branches/writeconcomillas/Compilador/src/comunicacion/Modelo.java
+++
@@ -1,9 +0,0 @@
-package comunicacion;
-
-import comunicacion.transfers.Transfer;
-
-public interface Modelo {
-
-	void ejecutar(Transfer trans);
-
-}
--- branches/writeconcomillas/tests2/Buenos/pruebasIf.pas
+++
@@ -1,8 +0,0 @@
-program pruebasIf;
-var
-x:boolean;
-
-begin
-	x:=cierto;
-	write(x);
-end.
